b990648f0e408b7392f533bcb152d7233fda4069
protae5544 <98496565+protae5544@users.noreply.github.com>
2025-07-22 04:35:21 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ['puppeteer']
+  },
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
+  }
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
